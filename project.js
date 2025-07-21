515cc747e870070819d3c87d92db9d6b5c05f21d
kbarker0201 <kbarker0201@gmail.com>
2025-07-20 20:32:21 -0500

    final changes

--- project.js
+++ project.js
@@ -1,1 +1,16 @@
-p
+let allScores = [
+    [6, 8, 9], 
+    [6, 5, 7],
+    [8, 7, 9]
+];
+
+let theatreSeating = [
+    ["A1", "A2", "A3", "A4"], 
+    ["B1", "B2", "B3", "B4"],
+    ["C1", "C2", "C3", "C4"]
+];
+
+let contactsList = [
+    ["Alice", "ay123@gmail.com", "913-555-1234"],
+    ["Mark", "myoung@gmail.com", "913-555-4568"]
+];
